771463f647eb4598e0c6870a9c1ec00627a2bb64
Carlos <carlosfortunato811@gmail.com>
2026-08-10 22:19:07 -0300

    Questao 9

---
+++ Questao_9.java
@@ -0,0 +1,39 @@
+/*# Questão 9: Validação de Entrada com Múltiplos try/catch
+
+Implemente um programa que leia três números decimais do usuário e calcule a média aritmética. Utilize múltiplos blocos try/catch para tratar diferentes tipos de exceção 
+(NumberFormatException, InputMismatchException). Valide se os números são positivos com if/else. */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Questao_9{
+
+    public static void main(String[] args) {
+
+        Scanner Entradauser = new Scanner(System.in);
+
+        try {
+    System.out.println("Bem vindo ao calculador de média aritimética");    
+    System.out.println("Digite 3 números, que lhe trarei a média.");   
+
+    System.out.println("Digite o primeiro numero: ");      
+    double numero1 = Entradauser.nextDouble();
+    System.out.println("Digite o segundo numero: ");      
+    double numero2 = Entradauser.nextDouble();
+    System.out.println("Digite o terceiro numero: ");      
+    double numero3 = Entradauser.nextDouble();
+    
+
+    double media = (numero1 + numero2 + numero3) / 3;
+    System.out.println("A média aritmética é: " + media);
+        }
+    catch (InputMismatchException e) {
+    System.out.println("Erro: digite apenas valores numéricos válidos.");
+    }
+    catch (NumberFormatException e) {
+        System.out.println("Erro ao converter o valor para número");
+    }
+     Entradauser.close();
+}
+}
+
+//não terminei, falta os if e else
