ae759346020f1f9273c4ef7ffe386e838bb2c72a
Carlos <carlosfortunato811@gmail.com>
2026-08-17 21:55:56 -0300

    anotacoes

--- Questao_2vet.java
+++ Questao_2vet.java
@@ -11,7 +11,7 @@ public class Questao_2vet {
 
         try {
 
-            int[] numeros = new int[15];
+            int[] numeros = new int[15]; //essa aqui é minha array com limite de 15 
 
             int qtdPar = 0;
             int qtdImpar = 0;
@@ -19,14 +19,14 @@ public class Questao_2vet {
             for (int i = 0; i < numeros.length; i++) {
 
                 System.out.println("Digite o valor do " + (i + 1) + "º número:");
-                numeros[i] = Entradauser.nextInt();
+                numeros[i] = Entradauser.nextInt(); //entrada do usuário seguindo a lógica do i + 1 pra se referir ao número, pq o indice começa com 0
             }
 
             System.out.println("\nNúmeros pares:");
 
             for (int i = 0; i < numeros.length; i++) {
 
-                if (numeros[i] % 2 == 0) {
+                if (numeros[i] % 2 == 0) { //se o resto da divisão atribuir o vlaor 0, vai entrar para o contador qtdpar.
 
                     System.out.println(numeros[i]);
                     qtdPar++;
@@ -36,18 +36,18 @@ public class Questao_2vet {
 
             System.out.println("\nNúmeros ímpares:");
 
-            for (int i = 0; i < numeros.length; i++) {
+            for (int i = 0; i < numeros.length; i++) {  
 
-                if (numeros[i] % 2 != 0) {
+                if (numeros[i] % 2 != 0) { //mesma lógicapara os ímpares, mas com a adição do ponto de exclamação ao invés do "==", ou seja, adicionei o operador "diferente de"
 
-                    System.out.println(numeros[i]);
-                    qtdImpar++;
+                    System.out.println(numeros[i]); 
+                    qtdImpar++;   //contador qtdImpar ++
 
                 }
             }
 
-            System.out.println("\nQuantidade de números pares: " + qtdPar);
-            System.out.println("Quantidade de números ímpares: " + qtdImpar);
+            System.out.println("\nQuantidade de números pares: " + qtdPar); //aq ele chama o qtd de par
+            System.out.println("Quantidade de números ímpares: " + qtdImpar); //msm coisa soq para impar
 
         } catch (InputMismatchException e) {
 
